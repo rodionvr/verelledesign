82baba846a660ed29e0ad8b4e1e29520a51c30a0
varl0011 <varl0011@algonquinlive.com>
2026-02-05 00:54:40 -0500

    Adds changes

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useState } from "react";
-import { sendEmailAction } from "../app/actions/sendEmail";
 import React from "react";
 
 export default function ContactForm() {
